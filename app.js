5e879a17da5b3d14d7b0d7e4ebb7e620c3f483d5
Oh Junseok <y21zzp@gmail.com>
2023-06-22 15:43:49 +0900

    lv2 과제를 위한 커밋

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const connect = require('./schemas');
+const routes = require('./routes');
+
+const app = express();
+const port = 3000;
+
+connect();
+
+app.use(express.json());
+
+app.use('/api', routes);
+
+app.listen(port, () => {
+    console.log(port, '서버가 실행되었습니다.');
+});
